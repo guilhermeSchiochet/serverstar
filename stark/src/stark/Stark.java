3bd774fc16b95c6a99478c7029710842db0850f0
GUILHERME SCHIOCHET <guilherme.schiochet@alunos.sc.senac.br>
2021-08-03 21:08:37 -0300

    star

---
+++ stark/src/stark/Stark.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stark;
+
+/**
+ *
+ * @author guilherme.schiochet
+ */
+public class Stark {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+        
+        variavel val = new viriavel(); 
+        
+        
+       
+        
+        
+        
+        
+    
+     System.out.println("Calculo");
+     
+     System.out.println("|");
+     System.out.println("V");
+     
+       int a = 24; 
+                int k = 90; 
+                int o = 2;
+               int b = 12; 
+               int r = 54;
+               
+               
+     int result = a + k + o + b + r;
+     
+     System.out.println(a);
+    System.out.println("+");
+     System.out.println(k);
+       System.out.println("+");
+          System.out.println(o);
+             System.out.println("+");
+       System.out.println(b);
+    System.out.println("+");
+        System.out.println(r);
+    
+    System.out.println("=");
+    
+    
+    System.out.printf( "Soma da variavel a k o b r = \\d \n", (val.a + val.b));
+    if(ListNum[0] + ListNum[6] == resp) (
+            
+            System.out.print("resposta correta " + (ListNum[6] - ListNum[0]))
+            
+                                   );
+     
+    
+    
+    
+    }
+    
+}
